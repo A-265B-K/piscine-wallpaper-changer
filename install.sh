007c8edd61e9d45ea8b7b40a52e5783ca67479d3
Alyssa Kuijten <alkuijte@f0r3s14.codam.nl>
2025-06-03 12:30:23 +0200

    1

--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@
 
 rm -rf $HOME/.kitty_wallp
 
-git clone git@github.com:A-265B-K/piscine-wallpaper-changer.git $HOME/.kitty_wallp
+git clone https://github.com/A-265B-K/piscine-wallpaper-changer.git $HOME/.kitty_wallp
 
-sleep 10
+sleep 4
 
 cp -v $HOME/.kitty_wallp/wallpaper.service $HOME/.config/systemd/user/
 
 
 systemctl --user daemon-reload
 systemctl --user restart wallpaper.service
+
+clear
 
 echo "Meow!"
