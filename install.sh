38a0160b875548d4b3198f9926bcbb0a4503df19
Alyssa Kuijten <alkuijte@f0r3s14.codam.nl>
2025-06-03 11:56:42 +0200

    1

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 # this script can also be found in /sgoinfre/ under the name of
 # ./wallptool_install
 
-rmdir $HOME/.kitty_wallp
+rm -rf $HOME/.kitty_wallp
 
 git clone git@github.com:A-265B-K/piscine-wallpaper-changer.git $HOME/.kitty_wallp
 
-
-
 sleep 10
 
-cp $HOME/.kitty_wallp/wallpaper.service $HOME/.config/systemd/user/
+cp -v $HOME/.kitty_wallp/wallpaper.service $HOME/.config/systemd/user/
+
+
+systemctl --user daemon-reload
+systemctl --user restart wallpaper.service
 
 echo "Meow!"
