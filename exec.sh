c099204a98f2d70f8b1db58b54b6dcd36f4b4681
Alyssa Kuijten <alkuijte@f0r3s14.codam.nl>
2025-06-03 12:04:11 +0200

    1

--- exec.sh
+++ exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR = "$HOME/.wallpaper/wallpapers"
+DIR="$HOME/.kitty_wallp/wallpapers"
 
 IMG=($(find "$DIR" -type f \( -name '*.jpg' -o -iname '*.png' -o -iname '*.gif' \)))
 
@@ -8,9 +8,8 @@ if [ ${#IMG[@]} -eq 0 ]; then
 	exit 1
 fi
 
-R_IMG="${IMG[RANDOM % ${#IMG[@]}"
+R_IMG="${IMG[RANDOM % ${#IMG[@]}]}"
 
 
 gsettings set org.gnome.desktop.background picture-uri "file://$R_IMG"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$R_IMG"
-
